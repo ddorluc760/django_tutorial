0d2c338e72b322eea821915fc9b5e059a47f3c74
ddorluc760 <ddorluc760@iesalmudeyne.es>
2025-12-17 23:19:30 +0000

    Cambio de imagen de fondo

--- django_tutorial/settings.py
+++ django_tutorial/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,5 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField' 
